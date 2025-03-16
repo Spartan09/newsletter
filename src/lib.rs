0302038667799577897741856d08515015adaa96
Spartan09 <ayushsg0412@gmail.com>
2025-03-16 17:57:28 +0530

    Use configuration variable for server address

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ async fn health_check() -> impl Responder {
 
 pub fn run() -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
+        .bind(address)?
         .run();
 
     Ok(server)
