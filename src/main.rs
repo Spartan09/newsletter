b13cd4bcbba86984fb100cdc10d7f6d9073833e3
Spartan09 <ayushsg0412@gmail.com>
2025-03-17 08:24:10 +0530

    Add configuration loading functionality

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::net::TcpListener;
 
-use newsletter::startup;
+use newsletter::configuration::get_configuration;
+use newsletter::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000")?;
-    startup::run(listener)?.await
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
