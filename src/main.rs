713d98611b55589b149a7a9c74bfcdc587c7ad0a
Spartan09 <ayushsg0412@gmail.com>
2025-03-18 15:16:36 +0530

    Add telemetry initialization to main function

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ use std::net::TcpListener;
 
 use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
+use newsletter::telemetry::{get_subscriber, init_subscriber};
 use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber("newsletter".into(), "info".into());
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
