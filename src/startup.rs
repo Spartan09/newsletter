1e0ac84061ab7e8c66fa2f72585247534ae34601
Spartan09 <ayushsg0412@gmail.com>
2025-03-18 23:40:53 +0530

    Replace Logger with TracingLogger for request monitoring

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,8 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, middleware::Logger, web, App, HttpServer};
+use actix_web::{App, HttpServer, dev::Server, web};
 use sqlx::PgPool;
+use tracing_actix_web::TracingLogger;
 
 use crate::routes;
 
@@ -9,7 +10,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
             .app_data(db_pool.clone())
