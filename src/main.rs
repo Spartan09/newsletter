d0e954c9de269bd748424a2640be97135cf4e440
Spartan09 <ayushsg0412@gmail.com>
2025-03-21 08:29:35 +0530

    Remove secrecy::ExposeSecret and use connect_options method

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use std::net::TcpListener;
 use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
 use newsletter::telemetry::{get_subscriber, init_subscriber};
-use secrecy::ExposeSecret;
 use sqlx::PgPool;
 
 #[tokio::main]
@@ -12,9 +11,7 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool =
-        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to Postgres.");
+    let connection_pool = PgPool::connect_lazy_with(configuration.database.connect_options());
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
